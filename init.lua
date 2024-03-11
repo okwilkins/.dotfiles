7ee9c984c19e6739bb781b5c73f78cd0ab7f8afb
Oliver Wilkins <okwilkins@googlemail.com>
2024-03-11 11:37:00 +0000

    Yank and paste for WSL

--- init.lua
+++ init.lua
@@ -29,6 +29,22 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
+-- This makes yanking and pasting easier on WSL
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
 -- Enable break indent
 vim.opt.breakindent = true
 
