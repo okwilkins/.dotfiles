cded630e243b37220fafc2dbc53aae6cec59e592
Oliver Wilkins <okwilkins@googlemail.com>
2024-06-07 21:36:31 +0100

    improve: Remove neotree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,18 +7,6 @@ return {
     end,
   },
 
-  -- File explorer tree
-  {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v3.x",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-      }
-  },
-
   -- Highlight todo, notes, etc in comments
   {
     'folke/todo-comments.nvim',
