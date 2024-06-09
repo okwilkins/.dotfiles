69b291a02e7d87e9f91b4d467e21af93e634a408
Oliver Wilkins <okwilkins@googlemail.com>
2024-06-09 16:25:29 +0100

    refactor(install): Reorder installing

--- install.sh
+++ install.sh
@@ -19,6 +19,14 @@ do
 done
 
 
+# Neovim
+# NOTE: This won't work on MacOS only only Linux
+curl -LO -s https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+
+
 # Zsh setup
 # Install zsh
 sudo apt install zsh -y
@@ -28,12 +36,4 @@ chsh -s $(which zsh)
 
 # Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
-# Neovim
-# NOTE: This won't work on MacOS only only Linux
-curl -LO -q https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
-rm nvim-linux64.tar.gz
 
