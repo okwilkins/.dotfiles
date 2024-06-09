47943be2972836cfd47bdb85ce5c8fb51d1a7af8
Oliver Wilkins <okwilkins@googlemail.com>
2024-06-09 15:01:27 +0100

    feat: Install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Zsh setup
+# Install zsh
+sudo apt install zsh
+
+# Make zsh the default shell
+chsh -s $(which zsh)
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+# Neovim
+# NOTE: This won't work on MacOS only only Linux
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+rm nvim-linux64.tar.gz
+
