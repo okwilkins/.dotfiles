ba143bd00d82ba14c67030a2a8f803de3fb5c9d5
Oliver Wilkins <okwilkins@googlemail.com>
2024-06-15 20:17:32 +0100

    feat: Add Ruff LSP for Python linting

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -72,3 +72,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 local lspconfig = require('lspconfig')
 lspconfig.lua.setup({})
 lspconfig.gleam.setup({})
+lspconfig.ruff_lsp.setup({
+  init_options = {
+    settings = {
+      -- Any extra CLI arguments for `ruff` go here.
+      args = {},
+    }
+  }
+})
