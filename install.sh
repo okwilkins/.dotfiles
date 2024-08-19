72329c512d77510ef18a9940d698940d34646966
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-19 22:02:45 +0000

    feat(install): Stow install and stowing

--- install.sh
+++ install.sh
@@ -8,3 +8,52 @@ wget --quiet --output-document=/dev/stdout https://get.jetify.com/devbox | bash 
 
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+
+
+# Install Stow
+install_stow_debian() {
+    echo "Detected Debian/Ubuntu. Installing Stow..."
+    sudo apt update
+    sudo apt install -y stow
+}
+
+install_stow_fedora() {
+    echo "Detected Fedora. Installing Stow..."
+    sudo dnf install -y stow
+}
+
+install_stow_arch() {
+    echo "Detected Arch Linux. Installing Stow..."
+    sudo pacman -Syu --noconfirm stow
+}
+
+install_stow_macos() {
+    echo "Detected macOS. Installing Stow..."
+    brew install stow
+}
+
+# Determine the operating system and call the appropriate function
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if command -v apt &> /dev/null; then
+        install_stow_debian
+    elif command -v dnf &> /dev/null; then
+        install_stow_fedora
+    elif command -v pacman &> /dev/null; then
+        install_stow_arch
+    else
+        echo "Unsupported Linux distribution."
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    install_stow_macos
+else
+    echo "Unsupported operating system."
+fi
+
+
+# Stow dotfiles
+cd configs
+for dir in */; do
+    echo "Stowing directory: $dir"
+    stow -D $dir
+    stow $dir -t ~
+done
