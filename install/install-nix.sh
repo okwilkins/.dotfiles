31a34bba62cae9c10c77b1c5979335654e6c35d0
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-31 17:58:21 +0100

    feat(install): Symlink flake.nix

--- install/install-nix.sh
+++ install/install-nix.sh
@@ -3,3 +3,5 @@
 mkdir -p "$XDG_CONFIG_HOME/nix"
 
 ln -sf "$DOTFILES/configs/nix/nix.conf" "$XDG_CONFIG_HOME/nix/nix.conf"
+ln -sf "$DOTFILES/configs/nix/flake.nix" "$XDG_CONFIG_HOME/nix/flake.nix"
+
