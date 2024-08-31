bddc8c70eb52515eb27df05df28bbf55cbca530f
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-31 23:16:48 +0100

    refactor: Move to home-manager flake system

--- install/install-nix-linux.sh
+++ install/install-nix-linux.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 mkdir -p "$XDG_CONFIG_HOME/nix"
-mkdir -p "$XDG_CONFIG_HOME/nix-linux"
 mkdir -p "$XDG_CONFIG_HOME/home-manager"
 
 ln -sf "$DOTFILES/configs/nix/nix.conf" "$XDG_CONFIG_HOME/nix/nix.conf"
-ln -sf "$DOTFILES/configs/nix-linux/flake.nix" "$XDG_CONFIG_HOME/home-manager/flake.nix"
-ln -sf "$DOTFILES/configs/nix-linux/flake.lock" "$XDG_CONFIG_HOME/home-manager/flake.lock"
-ln -sf "$DOTFILES/configs/nix-linux/home.nix" "$XDG_CONFIG_HOME/home-manager/home.nix"
+ln -sf "$DOTFILES/configs/home-manager/flake.nix" "$XDG_CONFIG_HOME/home-manager/flake.nix"
+ln -sf "$DOTFILES/configs/home-manager/flake.lock" "$XDG_CONFIG_HOME/home-manager/flake.lock"
+ln -sf "$DOTFILES/configs/home-manager/home.nix" "$XDG_CONFIG_HOME/home-manager/home.nix"
 
