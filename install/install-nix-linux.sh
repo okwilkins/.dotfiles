923e347217e5e35ce5cec34f6aba1feba24da811
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-31 20:26:39 +0100

    refactor: home-manager install correction

--- install/install-nix-linux.sh
+++ install/install-nix-linux.sh
@@ -2,10 +2,12 @@
 
 mkdir -p "$XDG_CONFIG_HOME/nix"
 mkdir -p "$XDG_CONFIG_HOME/nix-linux"
+mkdir -p "$XDG_CONFIG_HOME/home-manager"
 
 ln -sf "$DOTFILES/configs/nix/nix.conf" "$XDG_CONFIG_HOME/nix/nix.conf"
 ln -sf "$DOTFILES/configs/nix-linux/flake.nix" "$XDG_CONFIG_HOME/nix-linux/flake.nix"
 ln -sf "$DOTFILES/configs/nix-linux/flake.lock" "$XDG_CONFIG_HOME/nix-linux/flake.lock"
+ln -sf "$DOTFILES/configs/nix-linux/home.nix" "$XDG_CONFIG_HOME/home-manager/home.nix"
 
 
 # Install Nix home-manager
