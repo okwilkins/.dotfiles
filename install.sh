bdcfb21277148aab1084a4b83bb43c14b1189a86
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-31 16:32:52 +0100

    refactor: Move Nix installation

--- install.sh
+++ install.sh
@@ -4,16 +4,29 @@ sudo -v
 source ./configs/zsh/zshenv
 
 echo "Installing Nix"
-. "$DOTFILES/install/install-nix.sh"
+# Install Nix package manager
+echo "Installing Nix package manager"
+if ! command -v nix &> /dev/null; then
+    sh <(curl -sL https://nixos.org/nix/install) --daemon --yes
+else
+    echo "Nix is already installed! Skipping installation..."
+fi
 
-echo "Installing zsh"
-. "$DOTFILES/install/install-zsh.sh"
+# Install Nix home-manager
+echo "Installing Nix home-manager"
+if ! command -v home-manager &> /dev/null; then
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update
+    nix-shell '<home-manager>' -A install
+else
+    echo "Nix home-manager is already installed! Skipping installation..."
+fi
 
 # Make zsh the default shell
 # Allows root to change shell without a password
-sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
-command -v $DEVBOX_GLOBAL_BIN/zsh | sudo tee -a /etc/shells
+# sudo sed s/required/sufficient/g -i /etc/pam.d/chsh
+# command -v $DEVBOX_GLOBAL_BIN/zsh | sudo tee -a /etc/shells
 
-chsh -s $DEVBOX_GLOBAL_BIN/zsh $(whoami)
+# chsh -s $DEVBOX_GLOBAL_BIN/zsh $(whoami)
 
-exec $DEVBOX_GLOBAL_BIN/zsh
+# exec $DEVBOX_GLOBAL_BIN/zsh
--- install/install-nix.sh
+++ install/install-nix.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
 
-# Install Nix package manager
-echo "Installing Nix package manager"
-if ! command -v nix &> /dev/null; then
-    sh <(curl -sL https://nixos.org/nix/install) --daemon --yes
-else
-    echo "Nix is already installed! Skipping installation..."
-fi
-
-# Install Nix home-manager
-echo "Installing Nix home-manager"
-if ! command -v home-manager &> /dev/null; then
-    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-    nix-channel --update
-    nix-shell '<home-manager>' -A install
-else
-    echo "Nix home-manager is already installed! Skipping installation..."
-fi
-
