370f9472c1394a75c44554118725cd010f19b950
Oliver Wilkins <okwilkins@googlemail.com>
2024-08-31 19:41:59 +0100

    refactor: Nix dirs

--- install/install-nix.sh
+++ install/install-nix.sh
@@ -3,6 +3,6 @@
 mkdir -p "$XDG_CONFIG_HOME/nix"
 mkdir -p "$XDG_CONFIG_HOME/nix-linux"
 
-ln -sf "$DOTFILES/configs/nix-linux/nix.conf" "$XDG_CONFIG_HOME/nix/nix.conf"
+ln -sf "$DOTFILES/configs/nix/nix.conf" "$XDG_CONFIG_HOME/nix/nix.conf"
 ln -sf "$DOTFILES/configs/nix-linux/flake.nix" "$XDG_CONFIG_HOME/nix-linux/flake.nix"
 
