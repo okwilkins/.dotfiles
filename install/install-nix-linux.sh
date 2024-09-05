1d2e7a8c90a228af4bf7c738c08c2c6ac30c9ee1
Oliver Wilkins <okwilkins@googlemail.com>
2024-09-05 20:18:19 +0000

    feat: Nix build and source files

--- install/install-nix-linux.sh
+++ install/install-nix-linux.sh
@@ -8,3 +8,8 @@ ln -sf "$DOTFILES/configs/nix-linux/flake.nix" "$XDG_CONFIG_HOME/nix-linux/flake
 ln -sf "$DOTFILES/configs/nix-linux/flake.lock" "$XDG_CONFIG_HOME/nix-linux/flake.lock"
 ln -sf "$DOTFILES/configs/nix-linux/home.nix" "$XDG_CONFIG_HOME/nix-linux/home.nix"
 
+
+cd $XDG_CONFIG_HOME/nix-linux
+nix run . -- build --flake .
+ln -sf $(readlink result) "$XDG_CONFIG_HOME/nix-config"
+rm result
