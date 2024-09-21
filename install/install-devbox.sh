39b4cc3a7c1683d4d99c13a6e02a0fbc8e6d1272
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-21 18:37:11 +0100

    feat(tmux): Add plugin defs + new keybinds

--- install/install-devbox.sh
+++ install/install-devbox.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install devbox
-DEVBOX_USE_VERSION=0.12.0
-wget --quiet --output-document=/dev/stdout https://get.jetify.com/devbox | bash -s -- -f
+DEVBOX_USE_VERSION=0.13.0
+curl --proto '=https' -s -L https://get.jetify.com/devbox | bash -s -- -f
 
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -3,3 +3,4 @@
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 
 ln -sf "$DOTFILES/configs/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
+ln -sf "$DOTFILES/configs/tmux/tmux.reset.conf" "$XDG_CONFIG_HOME/tmux/tmux.reset.conf"
