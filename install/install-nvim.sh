ac1baf25eefc202a7a1c6eac47d80e29b1459cd2
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-21 20:25:58 +0100

    feat(nvim): Symlink lazy-lock

--- install/install-nvim.sh
+++ install/install-nvim.sh
@@ -2,3 +2,5 @@
 
 mkdir -p "$XDG_CONFIG_HOME/nvim"
 cp -rsv "$DOTFILES/configs/nvim/" "$XDG_CONFIG_HOME/nvim/"
+ln -sF "$XDG_CONFIG_HOME/nvim/lazy-lock.json" "$DOTFILES/configs/nvim/lazy-lock.json"
+
