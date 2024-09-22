fedba138d7a2ddad83ec4cbbbf5f2bce24b4a9d8
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-22 19:20:16 +0100

    feat(nvim): Add UFO for nice folds

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -119,5 +119,46 @@ return {
         event = "UIEnter",
         opts = { useDefaultKeymaps = true },
     },
+
+    -- Makes folds nicer
+    {
+        "kevinhwang91/nvim-ufo",
+        event = "UIEnter",
+        dependencies = {
+            "kevinhwang91/promise-async",
+        },
+        keys = {
+            { "zM", function() require("ufo").closeAllFolds() end, desc = "󱃄 Close All Folds" },
+            { "zR", function() require("ufo").openFoldsExceptKinds {} end, desc = "󱃄 Open All Folds" },
+        },
+        init = function()
+            -- INFO fold commands usually change the foldlevel, which fixes folds, e.g.
+            -- auto-closing them after leaving insert mode, however ufo does not seem to
+            -- have equivalents for zr and zm because there is no saved fold level.
+            -- Consequently, the vim-internal fold levels need to be disabled by setting
+            -- them to 99.
+            vim.opt.foldlevel = 99
+            vim.opt.foldlevelstart = 99
+            vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+            vim.o.foldcolumn = '1'
+        end,
+        opts = {
+            provider_selector = function(_, ft, _)
+                -- INFO some filetypes only allow indent, some only LSP, some only
+                -- treesitter. However, ufo only accepts two kinds as priority,
+                -- therefore making this function necessary :/
+                local lspWithOutFolding = { 'markdown', 'sh', 'css', 'html', 'python' }
+                if vim.tbl_contains(lspWithOutFolding, ft) then
+                    return { 'treesitter', 'indent' }
+                end
+                return { 'lsp', 'indent' }
+            end,
+            -- open opening the buffer, close these fold kinds
+            -- use `:UfoInspect` to get available fold kinds from the LSP
+            close_fold_kinds_for_ft = { lsp = { 'imports', 'comment' } },
+            open_fold_hl_timeout = 800,
+            fold_virt_text_handler = foldTextFormatter,
+        },
+    },
 }
 
