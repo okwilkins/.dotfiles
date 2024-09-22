21e049bb6c3a55159e5bc2df47077a460f0cfae4
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-22 21:02:05 +0100

    improve(nvim): Clean up setup for ufo

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -143,9 +143,9 @@ return {
         },
         event = "BufReadPost",
         opts = {
-          provider_selector = function()
-            return { "treesitter", "indent" }
-          end,
+          -- provider_selector = function()
+          --   return { "treesitter", "indent" }
+          -- end,
           open_fold_hl_timeout = 150,
           close_fold_kinds_for_ft = {
               default = {'imports', 'comment'},
@@ -192,28 +192,8 @@ return {
             table.insert(newVirtText, {suffix, 'MoreMsg'})
             return newVirtText
           end,
-          setup = {
-              fold_settings = function()
-                vim.keymap.set("n", "zR", function()
-                  require("ufo").openAllFolds()
-                end)
-                vim.keymap.set("n", "zM", function()
-                  require("ufo").closeAllFolds()
-                end)
-                vim.o.foldcolumn = "1" -- '0' is not bad
-                vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-                vim.o.foldlevelstart = 99
-                vim.o.foldenable = true
-                vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-              end
-          }
         },
       },
-      -- Folding preview, by default h and l keys are used.
-      -- On first press of h key, when cursor is on a closed fold, the preview will be shown.
-      -- On second press the preview will be closed and fold will be opened.
-      -- When preview is opened, the l key will close it and open fold. In all other cases these keys will work as usual.
-      { "anuvyklack/fold-preview.nvim", dependencies = "anuvyklack/keymap-amend.nvim", config = true },
     },
 }
 
