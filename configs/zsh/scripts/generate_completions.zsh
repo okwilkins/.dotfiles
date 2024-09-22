eebe95aa9cf0cb191beeb22c907809af4c5bf45c
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-22 13:23:36 +0100

    fix(zsh): fzf plugin proper loading

--- configs/zsh/scripts/generate_completions.zsh
+++ configs/zsh/scripts/generate_completions.zsh
@@ -4,5 +4,4 @@ docker completion zsh > $DOTFILES/configs/zsh/completions/_docker
 kubectl completion zsh > $DOTFILES/configs/zsh/completions/_kubectl 
 devbox completion zsh > $DOTFILES/configs/zsh/completions/_devbox
 zoxide init zsh > $DOTFILES/configs/zsh/completions/_zoxide 
-fzf --zsh > $DOTFILES/configs/zsh/completions/_fzf
 
