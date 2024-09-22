451668b5f4afc641ab83a53c932413dadee502d2
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2024-09-22 11:41:37 +0100

    refactor(zsh): Autocompletions and plugins

---
+++ configs/zsh/scripts/generate_completions.zsh
@@ -0,0 +1,8 @@
+# Generates new completions found in the zsh/completions directory
+
+docker completion zsh > $DOTFILES/configs/zsh/completions/_docker
+kubectl completion zsh > $DOTFILES/configs/zsh/completions/_kubectl 
+devbox completion zsh > $DOTFILES/configs/zsh/completions/_devbox
+zoxide init zsh > $DOTFILES/configs/zsh/completions/_zoxide 
+fzf --zsh > $DOTFILES/configs/zsh/completions/_fzf
+
--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -4,3 +4,7 @@ mkdir -p "$ZDOTDIR"
 
 ln -sf "$DOTFILES/configs/zsh/zshenv" "$HOME/.zshenv"
 ln -sf "$DOTFILES/configs/zsh/zshrc" "$ZDOTDIR/.zshrc"
+
+# Generate new completions
+zsh $DOTFILES/configs/zsh/scripts/generate_completions.zsh
+
