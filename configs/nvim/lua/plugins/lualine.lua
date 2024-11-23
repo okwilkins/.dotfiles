4d3b8ef807de2703ac7b05e5ef83fe3c7d89db32
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-23 23:21:00 +0000

    improve(nvim): Add lualine improvements

--- configs/nvim/lua/plugins/lualine.lua
+++ configs/nvim/lua/plugins/lualine.lua
@@ -2,8 +2,38 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+        sections = {
+            lualine_a = {'mode'},
+            lualine_b = {
+                { 'branch' },
+                {
+                    'diff',
+                    symbols = {added = ' ', modified = ' ', removed = ' '},
+                },
+                { 'diagnostics' },
+            },
+            lualine_c = {'filename'},
+            lualine_x = {'encoding', 'fileformat', 'filetype'},
+            lualine_y = {'progress'},
+            lualine_z = {'location'},
+        },
+        always_show_tabline = true,
         tabline = {
-            lualine_a = {'buffers'}, -- Display buffers in the tabline
+            lualine_a = {
+                {
+                    'buffers',
+                    symbols = {
+                        modified = ' ',
+                        alternate_file = '',
+                        directory = ' ',
+                    },
+                },
+            },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {}
         },
     },
 }
