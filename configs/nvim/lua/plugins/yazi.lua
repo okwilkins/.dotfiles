ce8e93442143fc999418c125a14e6c4ddaffe3cb
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-24 16:33:14 +0000

    improve(nvim): Move YAzi options out

--- configs/nvim/lua/plugins/yazi.lua
+++ configs/nvim/lua/plugins/yazi.lua
@@ -1,11 +1,5 @@
 return {
     "mikavilpas/yazi.nvim",
     event = "VeryLazy",
-    opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
-        open_for_directories = false,
-        keymaps = {
-            show_help = "<f1>",
-        },
-    },
+    opts = require("config.plugins.yazi"),
 }
