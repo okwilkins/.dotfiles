f75328a833238f9a9d5711df7ba674fba60c8dd9
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-24 13:03:15 +0000

    style(nvim): Remove line

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -39,7 +39,6 @@ map("n", "<leader>x", ":bd<CR>", { noremap = true, silent = true,  desc = "Close
 map("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true, desc = "Switch to next buffer" })
 map("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Switch to previous buffer" })
 
-
 -- Vim Tmux navigation
 vim.api.nvim_set_keymap("n", "<C-h>", "<cmd> TmuxNavigateLeft<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<c-j>", "<cmd> TmuxNavigateDown<cr>", { noremap = true, silent = true })
