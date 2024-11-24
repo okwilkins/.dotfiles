6e9998b507dbc294dba8f2358ecc23d023298fae
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-24 00:23:20 +0000

    improve(nvim): Improve symbols and colours of lualine

--- configs/nvim/lua/plugins/lualine.lua
+++ configs/nvim/lua/plugins/lualine.lua
@@ -2,13 +2,18 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+        options = {
+            theme = "auto",
+            component_separators = '',
+            section_separators = '', 
+        },
         sections = {
             lualine_a = {'mode'},
             lualine_b = {
                 { 'branch' },
                 {
                     'diff',
-                    symbols = {added = ' ', modified = ' ', removed = ' '},
+                    symbols = {added = ' ', modified = ' ', removed = ' '},
                 },
                 { 'diagnostics' },
             },
@@ -22,10 +27,21 @@ return {
             lualine_a = {
                 {
                     'buffers',
+                      buffers_color = {
+                        -- https://github.com/catppuccin/catppuccin?tab=readme-ov-file#-palette
+                        active = { bg = '#1e1e2e', fg = '#89b4fa' },
+                        inactive = { bg = '#313244', fg = '#6c7086' },
+                      },
+                    section_separators = { left = '', right = '' },
                     symbols = {
-                        modified = ' ',
+                        modified = ' ●',
                         alternate_file = '',
                         directory = ' ',
+                    },
+                    filetype_names = {
+                        TelescopePrompt = ' ',
+                        lazygit = '  ',
+                        yazi = '  ',
                     },
                 },
             },
