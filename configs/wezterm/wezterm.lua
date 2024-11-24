c72c8df4e098e0fd658967b33cd780eaa47b87ec
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-24 12:53:56 +0000

    feat(wezterm): WSL support

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,5 +1,7 @@
 local wezterm = require("wezterm")
-return {
+
+-- Base config
+local config = {
     color_scheme = "Catppuccin Mocha",
     enable_tab_bar = false,
     font_size = 16.0,
@@ -28,3 +30,18 @@ return {
         },
     },
 }
+
+
+-- Add WSL config if running under WSL
+if wezterm.running_under_wsl() then
+    config.default_domain = "WSL:Ubuntu"
+    config.wsl_domains = {
+        {
+            name = "WSL:Ubuntu",
+            distribution = "Ubuntu",
+        },
+    }
+end
+
+
+return config
