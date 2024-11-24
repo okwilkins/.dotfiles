be5d570c3c4f1ae1804551c7d0893036364a4cbc
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-24 15:51:38 +0000

    improve(nvim): Move dashboard opts to config

---
+++ configs/nvim/lua/config/plugins/dashboard.lua
@@ -0,0 +1,19 @@
+return {
+    theme = "hyper",
+    config = {
+        week_header = {
+            enable = true,
+        },
+        shortcut = {
+            { desc = "󰚰 Lazy Update", group = "@property", action = "Lazy update", key = "u" },
+            {
+                icon = " ",
+                icon_hl = "@variable",
+                desc = "Files",
+                group = "Label",
+                action = "Yazi cwd",
+                key = "f",
+            },
+        },
+    },
+}
--- configs/nvim/lua/plugins/dashboard.lua
+++ configs/nvim/lua/plugins/dashboard.lua
@@ -1,23 +1,5 @@
 return {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
-    opts = {
-        theme = "hyper",
-        config = {
-            week_header = {
-                enable = true,
-            },
-            shortcut = {
-                { desc = "󰚰 Lazy Update", group = "@property", action = "Lazy update", key = "u" },
-                {
-                    icon = " ",
-                    icon_hl = "@variable",
-                    desc = "Files",
-                    group = "Label",
-                    action = "Yazi cwd",
-                    key = "f",
-                },
-            },
-        },
-    },
+    opts = require("config.plugins.dashboard"),
 }
