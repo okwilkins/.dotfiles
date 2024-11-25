311e849db6c125f3cd32196c83004b22780b7fbd
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-25 22:02:52 +0000

    feat(nvim): Add Catppuccin palette adjustments for Lualine

--- configs/nvim/lua/plugins/lualine.lua
+++ configs/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,22 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = require("config.plugins.lualine"),
+    config = function()
+        local M = require("config.plugins.lualine")
+        local O = require("catppuccin").options
+        local C = require("catppuccin.palettes").get_palette("mocha")
+        local custom_lualine = require("catppuccin.utils.lualine")("mocha")
+
+        -- https://github.com/catppuccin/nvim/blob/faf15ab0201b564b6368ffa47b56feefc92ce3f4/lua/catppuccin/utils/lualine.lua
+        local transparent_bg = O.transparent_background and "NONE" or C.surface0
+
+        custom_lualine.normal.c.bg = transparent_bg
+        custom_lualine.inactive.c.bg = transparent_bg
+        custom_lualine.inactive.c.bg = transparent_bg
+        custom_lualine.inactive.c.bg = transparent_bg
+
+        M.options.theme = custom_lualine
+
+        require("lualine").setup(M)
+    end,
 }
