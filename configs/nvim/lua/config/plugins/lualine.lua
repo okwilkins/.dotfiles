3f74f05214558336544c6138be948462ab81d2a0
Oliver Wilkins <okwilkins@googlemail.com>
2024-11-26 21:52:58 +0000

    feat(nvim): Add LSP status to Lualine + other looks

--- configs/nvim/lua/config/plugins/lualine.lua
+++ configs/nvim/lua/config/plugins/lualine.lua
@@ -1,3 +1,23 @@
+local function lsp_client_names()
+    local clients = vim.lsp.get_active_clients()
+    local buffer_clients = {}
+
+    -- Filter clients attached to the current buffer
+    for _, client in pairs(clients) do
+        if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+            table.insert(buffer_clients, client.name)
+        end
+    end
+
+    -- Return a comma-separated list of client names or a default value
+    if next(buffer_clients) == nil then
+        return "󰣖 LSP: Inactive"
+    end
+
+    local language_servers = table.concat(buffer_clients, ", ")
+    return string.format("󰣖 LSP: [%s]", language_servers)
+end
+
 return {
     options = {
         theme = "auto",
@@ -5,18 +25,43 @@ return {
         section_separators = "",
     },
     sections = {
-        lualine_a = { "mode" },
+        lualine_a = {
+            "mode",
+            -- color = { fg = "#ffffff", bg = "#0077cc", gui = "bold" },
+        },
         lualine_b = {
+            {
+                "filetype",
+                icon_only = true,
+                padding = { left = 1, right = 0 },
+            },
+            {
+                "filename",
+                padding = { left = 0, right = 1 },
+                symbols = {
+                    modified = "",
+                    readonly = "",
+                    unnamed = " 󱙄",
+                    newfile = " ",
+                },
+            },
+        },
+        lualine_c = {
             { "branch" },
             {
                 "diff",
                 symbols = { added = " ", modified = " ", removed = " " },
             },
-            { "diagnostics" },
         },
-        lualine_c = { "filename" },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
+        lualine_x = {
+            {
+                "diagnostics",
+            },
+            {
+                lsp_client_names,
+            },
+        },
+        lualine_y = { { "filename" }, "progress" },
         lualine_z = { "location" },
     },
     always_show_tabline = true,
