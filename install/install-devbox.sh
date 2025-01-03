7ae7cfc2fba85482d2bbbdccfa24d7f3d74ef31c
Oliver Wilkins <okwilkins@googlemail.com>
2025-01-03 22:14:15 +0000

    improve(devbox): Improve Nix install order + up versions of Nix and Devbox

--- install/install-devbox.sh
+++ install/install-devbox.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Install devbox
-DEVBOX_USE_VERSION=0.13.0
-curl --proto '=https' -s -L https://get.jetify.com/devbox | bash -s -- -f
-
 # Install Nix
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+NIX_USE_VERSION=0.32.2
+curl --proto '=https' -s -L "https://github.com/DeterminateSystems/nix-installer/releases/download/v$NIX_USE_VERSION/nix-installer.sh" | sh -s -- install --no-confirm
+
+# Install devbox
+DEVBOX_USE_VERSION=0.13.7
+curl --proto '=https' -s -L https://get.jetify.com/devbox | bash -s -- -f
 
 mkdir -p "$XDG_DATA_HOME/devbox/global/default"
 ln -sF "$DOTFILES/configs/devbox/devbox.json" "$XDG_DATA_HOME/devbox/global/default/devbox.json"
