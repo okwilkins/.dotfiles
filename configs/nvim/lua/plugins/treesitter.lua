44bfe949bb4aed64fcb51841da1fe742e5ceec97
Oliver Wilkins <okwilkins@googlemail.com>
2025-01-04 15:58:52 +0000

    feat(nvim): Treesitter + Treesitter objects configured

--- configs/nvim/lua/plugins/treesitter.lua
+++ configs/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 -- INFO: https://www.lazyvim.org/plugins/treesitter
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
     event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
@@ -20,49 +23,7 @@ return {
         { "<bs>", desc = "Decrement Selection", mode = "x" },
     },
     opts_extend = { "ensure_installed" },
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "dockerfile",
-            "go",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "printf",
-            "python",
-            "query",
-            "regex",
-            "rust",
-            "toml",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "xml",
-            "yaml",
-            "zig",
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
-    },
+    opts = require("config.plugins.treesitter"),
     config = function(_, opts)
         require("nvim-treesitter.configs").setup(opts)
     end,
