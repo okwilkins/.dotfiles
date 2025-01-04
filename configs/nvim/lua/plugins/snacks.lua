d6162aa193658053d37c9afce1817bc488e9456a
Oliver Wilkins <okwilkins@googlemail.com>
2025-01-04 13:16:13 +0000

    improve(nvim): Turn off indent vis for dashboard

--- configs/nvim/lua/plugins/snacks.lua
+++ configs/nvim/lua/plugins/snacks.lua
@@ -7,7 +7,19 @@ return {
             fps = 120,
         },
         quickfile = { enabled = true },
-        indent = { enabled = true },
+        indent = {
+            enabled = true,
+            -- filter for buffers to enable indent guides
+            filter = function(buf)
+                return (
+                    vim.g.snacks_indent ~= false
+                    and vim.b[buf].snacks_indent ~= false
+                    and vim.bo[buf].buftype == ""
+                    -- Turn this off for the dashboard
+                    and vim.bo[buf].filetype ~= "dashboard"
+                )
+            end,
+        },
         notifier = { enabled = true },
         scroll = { enabled = true },
         statuscolumn = { enabled = true },
