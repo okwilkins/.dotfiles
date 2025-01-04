b512f0c8de7c22e3e690992ba4f1c7ce4e933d5e
Oliver Wilkins <okwilkins@googlemail.com>
2025-01-04 09:16:19 +0000

    feat(nvim): Add completions

---
+++ configs/nvim/lua/config/plugins/completion.lua
@@ -0,0 +1,50 @@
+local cmp_success, cmp = pcall(require, "cmp")
+local lspkind_success, lspkind = pcall(require, "lspkind")
+
+if not cmp_success then
+    return {}
+end
+
+local format = {}
+
+if lspkind_success then
+    format = lspkind.cmp_format()
+end
+
+return {
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        -- Add tab support
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<C-m>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true,
+        }),
+        -- If nothing is selected (including preselections) add a newline as usual on return key
+        ["<CR>"] = cmp.mapping({
+            i = function(fallback)
+                if cmp.visible() and cmp.get_active_entry() then
+                    cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                else
+                    fallback()
+                end
+            end,
+            s = cmp.mapping.confirm({ select = true }),
+            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        }),
+    },
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+    }),
+    formatting = {
+        format = format,
+    },
+}
---
+++ configs/nvim/lua/plugins/completion.lua
@@ -0,0 +1,13 @@
+return {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+    },
+    config = function()
+        local cmp = require("cmp")
+        cmp.setup(require("config.plugins.completion"))
+    end,
+}
