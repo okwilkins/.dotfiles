d5844ae137684224574ece0df4b2afd82455a9e9
Oliver Wilkins <okwilkins@googlemail.com>
2025-01-04 13:09:40 +0000

    feat(nvim): Snacks!!

---
+++ configs/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        animate = {
+            fps = 120,
+        },
+        quickfile = { enabled = true },
+        indent = { enabled = true },
+        notifier = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = true },
+    },
+}
