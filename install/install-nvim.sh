eb732065a97836f312b0a0dc7b3016e832255548
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2025-01-08 11:38:32 +0000

    fix: nvim config install

--- install/install-nvim.sh
+++ install/install-nvim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p "$XDG_CONFIG_HOME/nvim"
-cp -rsv "$DOTFILES/configs/nvim/" "$XDG_CONFIG_HOME/nvim/"
+cp -rsv "$DOTFILES/configs/nvim/" "$XDG_CONFIG_HOME"
 ln -F "$XDG_CONFIG_HOME/nvim/lazy-lock.json" "$DOTFILES/configs/nvim/lazy-lock.json"
 
