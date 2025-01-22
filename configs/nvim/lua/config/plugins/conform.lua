485c4fb9eea334851e651b1306d5509ef128d58e
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2025-01-22 15:29:21 +0000

    feat(nvim): Add Terrform conform

--- configs/nvim/lua/config/plugins/conform.lua
+++ configs/nvim/lua/config/plugins/conform.lua
@@ -2,6 +2,11 @@ return {
     formatters_by_ft = {
         lua = { "stylua" },
         yaml = { "yq" },
+        terraform = {
+            command = "terraform",
+            args = { "fmt", "." },
+            stdin = false,
+        },
     },
     format_on_save = {
         -- These options will be passed to conform.format()
