d29261de9cbe7977fbac8990a14d23aed064926f
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2025-06-02 21:10:31 +0100

    fix(install): Symlink zshenv to $ZDOTDIR

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -2,7 +2,7 @@
 
 mkdir -p "$ZDOTDIR"
 
-ln -sf "$DOTFILES/configs/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES/configs/zsh/zshenv" "$ZDOTDIR/.zshenv"
 ln -sf "$DOTFILES/configs/zsh/zshrc" "$ZDOTDIR/.zshrc"
 
 # Generate new completions
