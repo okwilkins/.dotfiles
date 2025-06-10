052c83a1637d5654c20cb46cd015828906bdc89b
Oliver Wilkins <okwilkins@googlemail.com>
2025-06-10 21:47:10 +0100

    refactor(tmux): reset -> keybinds name
    fix

--- install/install-tmux.sh
+++ install/install-tmux.sh
@@ -3,4 +3,4 @@
 mkdir -p "$XDG_CONFIG_HOME/tmux"
 
 ln -sf "$DOTFILES/configs/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf"
-ln -sf "$DOTFILES/configs/tmux/tmux.reset.conf" "$XDG_CONFIG_HOME/tmux/tmux.reset.conf"
+ln -sf "$DOTFILES/configs/tmux/tmux.keybinds.conf" "$XDG_CONFIG_HOME/tmux/tmux.keybinds.conf"
