c4398fd2aeac06fcb5ea536f44e05a92f4d4fd4c
Oliver Wilkins <okwilkins@googlemail.com>
2025-06-29 14:00:23 +0100

    feat: flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Oliver Wilkins' Nix configuration for both NixOS and MacOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = inputs: import ./outputs inputs;
+}
