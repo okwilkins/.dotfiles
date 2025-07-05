51c7700a7eb5d3256d4f2d5f4476a3b78b57a6f3
Oliver Wilkins <okwilkins@googlemail.com>
2025-07-05 21:42:41 +0100

    feat: Home-manager

---
+++ home/base/gui/default.nix
@@ -0,0 +1,6 @@
+{...}: {
+  imports [
+    ./terminals
+    ./browsers
+  ];
+}
---
+++ home/base/home.nix
@@ -0,0 +1,16 @@
+{pkgs, ...}: {
+  home = {
+    username = "oli";
+    homeDirectory = "/home/oli";
+
+    # This value determines the home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update home Manager without changing this value. See
+    # the home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "25.05";
+  };
+}
