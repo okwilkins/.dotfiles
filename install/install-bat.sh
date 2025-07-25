d500fd4fbee159217c2b2a58243043c2b0e11d98
Oliver Wilkins <okwilkins@googlemail.com>
2025-07-25 20:03:58 +0100

    refactor: catpuccin -> catppucin

--- install/install-bat.sh
+++ install/install-bat.sh
@@ -4,7 +4,7 @@ mkdir -p "$XDG_CONFIG_HOME/bat"
 mkdir -p "$XDG_CONFIG_HOME/bat/themes"
 
 ln -sf "$DOTFILES/configs/bat/config" "$XDG_CONFIG_HOME/bat/config"
-ln -sf "$DOTFILES/configs/bat/themes/catpuccin_mocha.tmTheme" "$XDG_CONFIG_HOME/bat/themes/Catppuccin Mocha.tmTheme"
+ln -sf "$DOTFILES/configs/bat/themes/catppuccin_mocha.tmTheme" "$XDG_CONFIG_HOME/bat/themes/Catppuccin Mocha.tmTheme"
 
 # Loads the themes
 bat cache --build
