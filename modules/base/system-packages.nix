490e0b94e4a8859488cbe6cc466d8e1f453fcd2a
Oliver Wilkins <okwilkins@googlemail.com>
2025-07-29 22:14:34 +0100

    refactor(modules): Use variables and factor out EDITOR

--- modules/base/system-packages.nix
+++ modules/base/system-packages.nix
@@ -1,8 +1,4 @@
 { pkgs, ... }: {
-  # For security reasons, do not load Neovim's user config
-  # since EDITOR may be used to edit some critical files
-  environment.variables.EDITOR = "nvim --clean";
-
   environment.systemPackages = with pkgs; [
     # Core tools
     neovim
--- modules/base/system-variables.nix
+++ modules/base/system-variables.nix
@@ -1,5 +1,8 @@
 { projectVars, ... }: {
-  environment.sessionVariables = {
+  environment.variables = {
+    # For security reasons, do not load Neovim's user config
+    # since EDITOR may be used to edit some critical files
+    EDITOR = "nvim --clean";
     XDG_CONFIG_HOME = projectVars.xdg.configHome;
     XDG_DATA_HOME = projectVars.xdg.dataHome;
     XDG_CACHE_HOME = projectVars.xdg.cacheHome;
