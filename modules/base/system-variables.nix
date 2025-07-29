48db0726bb06ed5f7b2e90d9e535cc9307904215
Oliver Wilkins <okwilkins@googlemail.com>
2025-07-29 22:26:48 +0100

    refactor: Move out more Zsh variables into system-variables

--- modules/base/system-variables.nix
+++ modules/base/system-variables.nix
@@ -1,10 +1,17 @@
 { projectVars, ... }: {
   environment.variables = {
+    # Editors
     # For security reasons, do not load Neovim's user config
     # since EDITOR may be used to edit some critical files
     EDITOR = "nvim --clean";
+    VISUAL = "nvim --clean";
+
+    # XDG
     XDG_CONFIG_HOME = projectVars.xdg.configHome;
     XDG_DATA_HOME = projectVars.xdg.dataHome;
     XDG_CACHE_HOME = projectVars.xdg.cacheHome;
+
+    # Zsh
+    ZDOTDIR = "${projectVars.xdg.configHome}/zsh";
   };
 }
