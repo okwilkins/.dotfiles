4e100d11e5f7a28a2a5b481ad8e0316533a5bd53
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-10 22:24:53 +0100

    feat(modules/linux): Disable xserver and enable greetd

--- modules/linux/desktop/system-packages.nix
+++ modules/linux/desktop/system-packages.nix
@@ -1,7 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, projectVars, ... }: {
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+  };
+
+  services = {
+    xserver.enable = false;
+    # https://nixos.wiki/wiki/Greetd
+    # Greetd is a minimal login manager
+    # It is primarily used to start Hyprland on login
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          user = projectVars.username;
+          command = "hyprland";
+        };
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
