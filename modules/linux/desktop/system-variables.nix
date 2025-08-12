f795e5e5a022abb877e211e1d90524c9d6c9e869
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-12 17:14:17 +0100

    feat(modules/linux/desktop): GTK_IM_MODULE system-variable

--- modules/linux/desktop/system-variables.nix
+++ modules/linux/desktop/system-variables.nix
@@ -2,5 +2,8 @@
   environment.sessionVariables = {
     # Hint Electron apps to use Wayland
     NIXOS_OZONE_WL = "1";
+    # Stops GTK apps to not capture shortcuts
+    # This is used to allow shift+ctrl+u for Ghostty
+    GTK_IM_MODULE = "none";
   };
 }
