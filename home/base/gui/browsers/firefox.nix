9e2c3b6daa559200df8147247041c83c4b489700
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-12 22:26:49 +0100

    feat(home/firefox): Install gwfox styling

--- home/base/gui/browsers/firefox.nix
+++ home/base/gui/browsers/firefox.nix
@@ -1,7 +1,35 @@
 # TODO: Remove this file when Zen working
-{ ... }:
+{ pkgs, ... }:
+let
+  gwfoxRev = "2.7.20";
+  gwfoxFirefoxCss = pkgs.fetchFromGitHub {
+    owner = "akkva";
+    repo = "gwfox";
+    rev = gwfoxRev;
+    sha256 = "sha256-fN0gDkIzp6lnUlZrgzCasCbMIMwO7Pc6LWJqikQkiRo=";
+  };
+in
 {
+  home.file.".mozilla/firefox/default/chrome" = {
+    source = "${gwfoxFirefoxCss}/chrome";
+    recursive = true;
+  };
   programs.firefox = {
     enable = true;
+    profiles.default = {
+      settings = {
+        "sidebar.revamp" = true;
+        "sidebar.verticalTabs" = true;
+        "sidebar.visibility" = "always";
+        "sidebar.main.tools" = "tabs";
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "svg.context-properties.content.enabled" = true;
+        "widget.windows.mica" = true;
+        "widget.windows.mica.toplevel-backdrop" = true;
+        "sidebar.animation.enabled" = false;
+        "widget.macos.native-context-menus" = false;
+        "gwfox.plus" = true;
+      };
+    };
   };
 }
