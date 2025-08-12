fa226cb11cbf6207da3c077fea373cca87fc70a1
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-12 22:07:40 +0100

    refactor(home/gui): Improve cursor import

--- home/linux/gui/cursor.nix
+++ home/linux/gui/cursor.nix
@@ -1,26 +1,18 @@
 {
   pkgs,
-  lib,
   projectVars,
   ...
 }:
 let
-  version = "0.3.2";
-  url = "https://github.com/ndom91/rose-pine-hyprcursor/releases/download/v${version}/rose-pine-cursor-hyprcursor_${version}.tar.gz";
-  cursor-archive = pkgs.fetchurl {
-    url = url;
-    sha256 = "5e84afe47ef723c465317bc58710f4e45b0b36366d4d88bf0325c712b711b58e";
+  rosePineCursorHyprcursor = pkgs.fetchFromGitHub {
+    owner = "ndom91";
+    repo = "rose-pine-hyprcursor";
+    rev = "v0.3.2";
+    sha256 = "sha256-ArUX5qlqAXUqcRqHz4QxXy3KgkfasTPA/Qwf6D2kV0U=";
   };
 in
 {
-  home.activation.extractCursor = lib.hm.dag.entryAfter [ "writeBoundry" ] ''
-    PATH=${
-      lib.makeBinPath [
-        pkgs.gnutar
-        pkgs.gzip
-      ]
-    }:$PATH
-    mkdir -p ${projectVars.xdg.dataDir}/icons/rose-pine-hyprcursor
-    tar -xzf ${cursor-archive} -C "${projectVars.xdg.dataDir}/icons/rose-pine-hyprcursor"
-  '';
+  home.file."${projectVars.xdg.dataDir}/icons/rose-pine-hyprcursor".source = "${
+    rosePineCursorHyprcursor
+  }";
 }
