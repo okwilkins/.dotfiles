f68df732e7488b2118baac63c87f2027089bf449
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-13 21:21:16 +0100

    feat(home/firefox): Expand settings

--- home/base/gui/browsers/firefox.nix
+++ home/base/gui/browsers/firefox.nix
@@ -1,34 +1,43 @@
 # TODO: Remove this file when Zen working
 { pkgs, ... }:
 let
-  gwfoxRev = "2.7.20";
-  gwfoxFirefoxCss = pkgs.fetchFromGitHub {
-    owner = "akkva";
-    repo = "gwfox";
-    rev = gwfoxRev;
-    sha256 = "sha256-fN0gDkIzp6lnUlZrgzCasCbMIMwO7Pc6LWJqikQkiRo=";
+  firefoxCss = pkgs.fetchFromGitHub {
+    owner = "QNetITQ";
+    repo = "WaveFox";
+    rev = "v1.204";
+    sha256 = "sha256-6QZyCiPqhYN/HChmQy3vtnCxHo77OaEgT/PvXs9K19w=";
   };
 in
 {
   home.file.".mozilla/firefox/default/chrome" = {
-    source = "${gwfoxFirefoxCss}/chrome";
+    source = "${firefoxCss}/chrome";
     recursive = true;
   };
   programs.firefox = {
     enable = true;
     profiles.default = {
       settings = {
+        # Personal settings
         "sidebar.revamp" = true;
         "sidebar.verticalTabs" = true;
         "sidebar.visibility" = "always";
         "sidebar.main.tools" = "tabs";
+        "browser.startup.homepage" = "about:blank";
+        "browser.startup.page" = 3; # Launch the last tab open
+        "browser.aboutConfig.showWarning" = false;
+        "browser.newtab.url" = "about:blank";
+        "signon.rememberSignons" = false; # Stops asking to save passwords
+        # Clean up new tab page
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+        "browser.newtabpage.activity-stream.feeds.highlights" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        # CSS settings
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "svg.context-properties.content.enabled" = true;
-        "widget.windows.mica" = true;
-        "widget.windows.mica.toplevel-backdrop" = true;
-        "sidebar.animation.enabled" = false;
-        "widget.macos.native-context-menus" = false;
-        "gwfox.plus" = true;
+        "layout.css.color-mix.enabled" = true;
+        "Style.WaveFox" = true;
+        "Style.Menu-Icons-Regular" = true;
       };
     };
   };
