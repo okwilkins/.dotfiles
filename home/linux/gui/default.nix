c94b4dd43ef5e1e66003d6216bb28faaa6701759
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-18 22:12:22 +0100

    refactor(linux/gui): Simplfy code structure

--- home/linux/gui/default.nix
+++ home/linux/gui/default.nix
@@ -1,11 +1,25 @@
-{ ... }:
+{ pkgs, projectVars, ... }:
+let
+  rosePineCursorHyprcursor = pkgs.fetchFromGitHub {
+    owner = "ndom91";
+    repo = "rose-pine-hyprcursor";
+    rev = "v0.3.2";
+    sha256 = "sha256-ArUX5qlqAXUqcRqHz4QxXy3KgkfasTPA/Qwf6D2kV0U=";
+  };
+in
 {
   imports = [
     ./hyprland
-    ./cursor.nix
     ./anyrun
-    ./overskride.nix
-    ./easyeffects.nix
     ./waybar
+  ];
+
+  home.file."${projectVars.xdg.dataDir}/icons/rose-pine-hyprcursor".source = "${
+    rosePineCursorHyprcursor
+  }";
+
+  home.packages = with pkgs; [
+    easyeffects
+    overskride
   ];
 }
