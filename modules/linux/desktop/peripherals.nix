aec7da7cdfeac15dc9c93a223f75d2e4ba0e9dee
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-18 23:01:26 +0100

    linux/desktop): Init airplay for peripherals

--- modules/linux/desktop/peripherals.nix
+++ modules/linux/desktop/peripherals.nix
@@ -10,5 +10,26 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+
+    # Airplay
+    # https://wiki.nixos.org/wiki/PipeWire#AirPlay/RAOP_configuration
+    # Opens UDP ports 6001-6002
+    raopOpenFirewall = true;
+
+    extraConfig.pipewire = {
+      "10-airplay" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-raop-discover";
+            # Increase the buffer size if you get dropouts/glitches
+            # args = {
+            #   "raop.latency.ms" = 500;
+            # };
+          }
+        ];
+      };
+    };
   };
+  # For Airplay discovery
+  services.avahi.enable = true;
 }
