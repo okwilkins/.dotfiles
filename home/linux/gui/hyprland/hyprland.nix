29a746663d2addfe1b5026ed437074d7bd0e12f2
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-28 21:18:12 +0100

    feat(hyprland): Hyprshot

--- home/linux/gui/hyprland/hyprland.nix
+++ home/linux/gui/hyprland/hyprland.nix
@@ -1,6 +1,9 @@
 { projectVars, pkgs, ... }:
 {
-  home.packages = with pkgs; [ hyprpaper ];
+  home.packages = with pkgs; [
+    hyprpaper
+    hyprshot
+  ];
   home.file."${projectVars.xdg.configDir}/hypr/" = {
     source = ./configs;
     recursive = true;
