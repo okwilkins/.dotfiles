52e1672ec51947d7e3dd74de7b1b1dd7191e1a28
Oliver Wilkins <okwilkins@googlemail.com>
2025-08-29 21:07:03 +0100

    feat(modules): Sudoer 30min timeout

--- modules/base/security.nix
+++ modules/base/security.nix
@@ -1,4 +1,10 @@
 {
+  security.sudo = {
+    # Set sudo timeout to 30mins
+    extraConfig = ''
+      Defaults timestamp_timeout=30
+    '';
+  };
   security.pki.certificates = [
     (builtins.readFile ../../certs/talos-homelab-ca.crt)
   ];
