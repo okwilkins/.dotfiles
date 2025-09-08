a75de466eae975e3ba8396e386151f5b8a09cca3
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-08 21:37:50 +0100

    feat(bottom): Catppuccin styling and config
    more

--- home/base/tui/bottom.nix
+++ home/base/tui/bottom.nix
@@ -1,8 +1,25 @@
-{ pkgs, projectVars, ... }:
+{
+  pkgs,
+  lib,
+  projectVars,
+  ...
+}:
+let
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "bottom";
+    rev = "eadd75acd0ecad4a58ade9a1d6daa3b97ccec07c";
+    hash = "sha256-dfukdk70ug1lRGADKBnvMhkl+3tsY7F+UAwTS2Qyapk=";
+  };
+  base = lib.importTOML "${catppuccinRepo}/themes/mocha.toml";
+  local = lib.importTOML ./bottom/bottom.toml;
+  merged = lib.recursiveUpdate base local;
+  tomlFormat = pkgs.formats.toml { };
+  bottomToml = tomlFormat.generate "bottom.toml" merged;
+in
 {
   home.packages = [ pkgs.bottom ];
-  home.file."${projectVars.xdg.configDir}/bottom" = {
-    source = ./bottom;
-    recursive = true;
+  home.file."${projectVars.xdg.configDir}/bottom/bottom.toml" = {
+    source = bottomToml;
   };
 }
