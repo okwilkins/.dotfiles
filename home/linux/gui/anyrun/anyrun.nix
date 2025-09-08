1ea86ae0dcd64bd5b6acf59b83d0767dc4662077
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-08 21:53:11 +0100

    feat(anyrun): Init configs

--- home/linux/gui/anyrun/anyrun.nix
+++ home/linux/gui/anyrun/anyrun.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, projectVars, ... }:
 {
   home.packages = with pkgs; [ anyrun ];
+  home.file."${projectVars.xdg.configDir}/anyrun" = {
+    source = ./config;
+    recursive = true;
+  };
 }
