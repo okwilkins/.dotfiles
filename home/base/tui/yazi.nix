c5d6fb54220710c6913c6b1e58a16b0a821780e7
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-08 22:10:48 +0100

    feat(yazi): Declaritively download Yazi Catppuccin theme

---
+++ home/base/tui/yazi.nix
@@ -0,0 +1,19 @@
+{
+  pkgs,
+  projectVars,
+  ...
+}:
+let
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "yazi";
+    rev = "043ffae14e7f7fcc136636d5f2c617b5bc2f5e31";
+    hash = "sha256-zkL46h1+U9ThD4xXkv1uuddrlQviEQD3wNZFRgv7M8Y=";
+  };
+in
+{
+  home.packages = [ pkgs.yazi ];
+  home.file."${projectVars.xdg.configDir}/yazi/theme.toml" = {
+    source = "${catppuccinRepo}/themes/mocha/catppuccin-mocha-blue.toml";
+  };
+}
