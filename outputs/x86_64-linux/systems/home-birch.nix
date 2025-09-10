017c93bd208de7ce7b645c172deb3e1ce64adfca
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-10 23:59:48 +0100

    fix(systems): Proper refs to base of repo

--- outputs/x86_64-linux/systems/home-birch.nix
+++ outputs/x86_64-linux/systems/home-birch.nix
@@ -1,16 +1,16 @@
 { nixpkgs, ... }:
 {
   imports = [
-    ../../../../home/base/core/cli-tools.nix
+    ../../../home/base/core/cli-tools.nix
     nixpkgs
   ];
   nixosConfigurations = {
     "birch" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ../../../../systems/home-birch
-        ../../../../modules/base
-        ../../../../home/base/home.nix
+        ../../../systems/home-birch
+        ../../../modules/base
+        ../../../home/base/home.nix
       ];
     };
   };
--- outputs/x86_64-linux/systems/home-oak.nix
+++ outputs/x86_64-linux/systems/home-oak.nix
@@ -1,16 +1,16 @@
 { nixpkgs, ... }:
 {
   imports = [
-    ../../../../home/base/core/cli-tools.nix
+    ../../../home/base/core/cli-tools.nix
     nixpkgs
   ];
   nixosConfigurations = {
     "oak" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ../../../../systems/home-oak
-        ../../../../modules/base
-        ../../../../home/base/home.nix
+        ../../../systems/home-oak
+        ../../../modules/base
+        ../../../home/base/home.nix
       ];
     };
   };
