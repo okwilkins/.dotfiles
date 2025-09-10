fe7fd745d089041726bf391ba4b017892c7ca9f8
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-10 18:35:50 +0100

    feat(k9s): Nix install Catppuccin skins to HOME

--- home/base/tui/k9s.nix
+++ home/base/tui/k9s.nix
@@ -1,8 +1,20 @@
 { pkgs, projectVars, ... }:
+let
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "k9s";
+    rev = "fdbec82284744a1fc2eb3e2d24cb92ef87ffb8b4";
+    hash = "sha256-9h+jyEO4w0OnzeEKQXJbg9dvvWGZYQAO4MbgDn6QRzM=";
+  };
+in
 {
   home.packages = [ pkgs.k9s ];
   home.file."${projectVars.xdg.configDir}/k9s" = {
     source = ./k9s;
+    recursive = true;
+  };
+  home.file."${projectVars.xdg.configDir}/k9s/skins" = {
+    source = "${catppuccinRepo}/dist";
     recursive = true;
   };
 }
