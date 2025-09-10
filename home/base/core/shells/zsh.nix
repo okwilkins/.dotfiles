e19f19c1d8fc66832474bbee9918ea19470175c7
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-10 18:50:56 +0100

    feat(zsh): Nix generation completion scripts

--- home/base/core/shells/zsh.nix
+++ home/base/core/shells/zsh.nix
@@ -1,9 +1,58 @@
 { pkgs, projectVars, ... }:
+let
+  gen-completion =
+    {
+      name,
+      command,
+      packages ? [ ],
+    }:
+    pkgs.runCommand "${name}-completion.zsh" {
+      nativeBuildInputs = packages;
+    } command;
+
+  dockerCompletion = gen-completion {
+    name = "docker";
+    command = "${pkgs.docker}/bin/docker completion zsh > $out";
+    packages = [ pkgs.docker ];
+  };
+  kubectlCompletion = gen-completion {
+    name = "kubectl";
+    command = "${pkgs.kubectl}/bin/kubectl completion zsh > $out";
+    packages = [ pkgs.kubectl ];
+  };
+  zoxideCompletion = gen-completion {
+    name = "zoxide";
+    command = "${pkgs.zoxide}/bin/zoxide init zsh --no-aliases > $out";
+    packages = [ pkgs.zoxide ];
+  };
+  # TODO: Install Dagger via its Flake as not available via pkgs
+  # daggerCompletion = gen-completion {
+  #   name = "dagger";
+  #   command = "${pkgs.dagger}/bin/dagger completion zsh > $out";
+  #   packages = [ pkgs.dagger ];
+  # };
+in
 {
   home.file."${projectVars.xdg.configDir}/zsh" = {
     source = ./zsh;
     recursive = true;
   };
+
+  # Completion scripts
+  home.file."${projectVars.xdg.configDir}/zsh/completions/docker-completion.sh" = {
+    source = dockerCompletion;
+  };
+  home.file."${projectVars.xdg.configDir}/zsh/completions/kubectl-completion.sh" = {
+    source = kubectlCompletion;
+  };
+  home.file."${projectVars.xdg.configDir}/zsh/completions/zoxide-completion.sh" = {
+    source = zoxideCompletion;
+  };
+  # home.file."${projectVars.xdg.configDir}/zsh/test/dagger-completion.sh" = {
+  #   source = daggerCompletion;
+  # };
+
+  # Zsh config files
   home.file."${projectVars.xdg.configDir}/zsh/.zshenv".text = ''
     # Zsh
     export HISTFILE="$ZDOTDIR/.zhistory"    # History filepath
