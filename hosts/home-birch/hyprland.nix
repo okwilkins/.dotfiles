41da157e9f4558db68ce3ffd2baa8991f939a5b1
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-11 00:24:48 +0100

    fix(birch): Hyprland importing

--- hosts/home-birch/hyprland.nix
+++ hosts/home-birch/hyprland.nix
@@ -1,7 +1,7 @@
 { projectVars, ... }:
 {
-  # imports = [ ../../home/base/home.nix ];
-  # home.file."${projectVars.xdg.configDir}/hypr/hypr-hardware.conf" = {
-  #   source = ./hypr-hardware.conf;
-  # };
+  imports = [ ../../home/base/home.nix ];
+  home.file."${projectVars.xdg.configDir}/hypr/hypr-hardware.conf" = {
+    source = ./hypr-hardware.conf;
+  };
 }
