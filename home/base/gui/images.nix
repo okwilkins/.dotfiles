f7d79618b491d7bb2c70fc5e25a2473d830ed580
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-11 21:33:25 +0100

    refactor(images): Use png for profile pic

--- home/base/gui/images.nix
+++ home/base/gui/images.nix
@@ -5,16 +5,16 @@ let
     sha256 = "sha256:1xmxf12q34rl3asj3y0kifh4icx4vp414rf08jra8xf5yc7m3z04";
   };
   profileHeadshot = builtins.fetchurl {
-    url = "https://drive.usercontent.google.com/download?id=1TlNca10Kw-_0Ratfc-b1zbPr94VtXV-C&export=download&authuser=0";
-    name = "headshot-profile.jpg";
-    sha256 = "sha256:0yah2i74p5j8xknv8fnxq2agidp65pv3653qrzvcmkrjw3gzfkkq";
+    url = "https://drive.usercontent.google.com/download?id=1XxqUGqaPZloubV_5cOlnUy7lxKiHEk7V&export=download&authuser=0";
+    name = "headshot-profile.png";
+    sha256 = "sha256:1lafzxg9q2xclxirwngaw75sbmdlpa6vhbiga8m8fkslb3hshgmc";
   };
 in
 {
   home.file."Pictures/wallpapers/leaf.jpg" = {
     source = wallpaper;
   };
-  home.file."Pictures/profiles/headshot-profile.jpg" = {
+  home.file."Pictures/profiles/headshot-profile.png" = {
     source = profileHeadshot;
   };
 }
