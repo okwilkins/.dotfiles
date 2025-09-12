59854584e44f486dd9407daf63cb793e0dbb2328
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-12 23:14:04 +0100

    feat(hyprland): Increase blur

---
+++ home/base/core/cli-tools/fzf.nix
@@ -0,0 +1,17 @@
+{ pkgs, projectVars, ... }:
+let
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "fzf";
+    rev = "7c2e05606f2e75840b1ba367b1f997cd919809b3";
+    hash = "sha256-fs3bOs1vfrtuono0yg1xjTSpzoS5m8ZRMD+CjAp+sDU=";
+  };
+in
+{
+  home.packages = with pkgs; [
+    fzf
+  ];
+  home.file."${projectVars.xdg.configDir}/zsh/pugins/catppuccin-fzf-mocha.sh" = {
+    source = "${catppuccinRepo}/themes/catppuccin-fzf-mocha.sh";
+  };
+}
