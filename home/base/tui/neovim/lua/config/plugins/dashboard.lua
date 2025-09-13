9aa937f7b00d5e2c2ce4e14062cbfabbbda7d475
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-13 14:25:29 +0100

    refactor(neovim): Rework dashboard to have splash logos and remove cluter

--- home/base/tui/neovim/lua/config/plugins/dashboard.lua
+++ home/base/tui/neovim/lua/config/plugins/dashboard.lua
@@ -1,19 +1,51 @@
-return {
-    theme = "hyper",
+local logo_dir = vim.fn.stdpath("config") .. "/lua/config/plugins/dashboard-logos"
+local fallback_logo = [[
+▀███▄   ▀███▀                             ██                   
+  ███▄    █                                                    
+  █ ███   █   ▄▄█▀██  ▄██▀██▄▀██▀   ▀██▀▀███ ▀████████▄█████▄  
+  █  ▀██▄ █  ▄█▀   ████▀   ▀██ ██   ▄█    ██   ██    ██    ██  
+  █   ▀██▄▓  ▓█▀▀▀▀▀▀██     ██  ██ ▄█     ▓█   ▓█    ██    ██  
+  ▓     ▓█▓  ▓█▄    ▄██     ▓█   ▓██      ▓█   ▓█    ▓█    ██  
+  ▓   ▀▓▓▓▓  ▓▓▀▀▀▀▀▀▓█     ▓▓   ▓█▓      ▓▓   ▓▓    ▓▓    ▓▓  
+  ▓     ▓▓▓  ▒▓▓     ▓▓▓   ▓▓▓   ▓▓▓      ▓▓   ▒▓    ▒▓    ▓▓  
+▒ ▒ ▒    ▒▓▓  ▒ ▒ ▒▒  ▒ ▒ ▒ ▒     ▒     ▒ ▒ ▒▒ ▒▓▒  ▒▒▒   ▒▒▓▒
+]]
+
+local function read_random_logo_lines(dir, fallback)
+    local list_str = vim.fn.glob(dir .. "/*")
+    local files = vim.split(list_str, "\n", { trimempty = true })
+
+    math.randomseed(os.time())
+    local lines
+    if #files > 0 then
+        local pick = files[math.random(#files)]
+        lines = vim.fn.readfile(pick)
+    else
+        lines = vim.split(fallback, "\n", { trimempty = false })
+    end
+
+    for _ = 1, 8 do
+        table.insert(lines, 1, "")
+    end
+    table.insert(lines, "") -- trailing blank line
+
+    return lines
+end
+
+local opts = {
+    theme = "doom",
+    hide = {
+        statusline = false,
+    },
     config = {
-        week_header = {
-            enable = true,
-        },
-        shortcut = {
-            { desc = "󰚰 Lazy Update", group = "@property", action = "Lazy update", key = "u" },
-            {
-                icon = " ",
-                icon_hl = "@variable",
-                desc = "Files",
-                group = "Label",
-                action = "Yazi cwd",
-                key = "f",
-            },
-        },
+        header = read_random_logo_lines(logo_dir, fallback_logo),
+        center = {},
+        footer = function()
+            local stats = require("lazy").stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+        end,
     },
 }
+
+return opts
