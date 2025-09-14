697a515bf5bb583132d981efeeac2152afaa8956
Oliver Wilkins <okwilkins@googlemail.com>
2025-09-14 15:51:32 +0100

    docs(swaync): Note on upgrading

--- home/linux/gui/swaync/swaync.nix
+++ home/linux/gui/swaync/swaync.nix
@@ -1,6 +1,7 @@
 { pkgs, projectVars, ... }:
 let
   catppuccinMochaSwaync = pkgs.fetchurl {
+    # Upgrade to 1.X when swaync is upgraded to 0.12+
     url = "https://github.com/catppuccin/swaync/releases/download/v0.2.3/mocha.css";
     hash = "sha256-Hie/vDt15nGCy4XWERGy1tUIecROw17GOoasT97kIfc=";
   };
