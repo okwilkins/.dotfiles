5f1da347c90e117fbee1d2d746cc0e487e2cfb25
Oliver Wilkins <oliver.wilkins@likezero.co.uk>
2025-10-16 20:23:28 +0100

    refactor(modules/nix): Use interval over dates for gc

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -6,7 +6,11 @@
   # Garbage collect weekly to keep disk space
   nix.gc = {
     automatic = true;
-    dates = "weekly";
+    interval = {
+      Weekday = 7;
+      Hour = 2;
+      Minute = 00;
+    };
     options = "--delete-older-than +7d";
   };
 }
