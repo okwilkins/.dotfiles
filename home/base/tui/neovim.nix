2db063510380e9df679afc4b1f703293689eea5c
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-28 19:45:42 +0000

    refactor(nvim): Out of store symlink spelling

--- home/base/tui/neovim.nix
+++ home/base/tui/neovim.nix
@@ -8,16 +8,15 @@
     source = ./neovim/lua;
     recursive = true;
   };
-  home.file."${osConfig.system.xdg.configDir}/nvim/spell" = {
-    source = ./neovim/spell;
-    recursive = true;
-  };
-  # Copy individual files from store
   home.file."${osConfig.system.xdg.configDir}/nvim/init.lua" = {
     source = ./neovim/init.lua;
   };
 
   home.file."${osConfig.system.xdg.configDir}/nvim/lazy-lock.json" = {
     source = config.lib.file.mkOutOfStoreSymlink "${osConfig.system.dotfilesDir}/home/base/tui/neovim/lazy-lock.json";
+  };
+  home.file."${osConfig.system.xdg.configDir}/nvim/spell" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${osConfig.system.dotfilesDir}/home/base/tui/neovim/spell";
+    recursive = true;
   };
 }
