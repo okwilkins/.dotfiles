5042a64def1410577e9a6f9b442c9647095bfd3c
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-28 19:41:50 +0000

    refactor(nvim): Dont recurse all files

--- home/base/tui/neovim.nix
+++ home/base/tui/neovim.nix
@@ -3,9 +3,18 @@
   programs.neovim = {
     enable = true;
   };
-  home.file."${osConfig.system.xdg.configDir}/nvim" = {
-    source = ./neovim;
+
+  home.file."${osConfig.system.xdg.configDir}/nvim/lua" = {
+    source = ./neovim/lua;
     recursive = true;
+  };
+  home.file."${osConfig.system.xdg.configDir}/nvim/spell" = {
+    source = ./neovim/spell;
+    recursive = true;
+  };
+  # Copy individual files from store
+  home.file."${osConfig.system.xdg.configDir}/nvim/init.lua" = {
+    source = ./neovim/init.lua;
   };
 
   home.file."${osConfig.system.xdg.configDir}/nvim/lazy-lock.json" = {
