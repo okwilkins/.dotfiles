04c82b21e85c35ef89570178eaf1b06a542adc88
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-28 19:32:42 +0000

    feat(nvim): Out of store link to dotfiles nvim lazy-lock

--- home/base/tui/neovim.nix
+++ home/base/tui/neovim.nix
@@ -1,4 +1,4 @@
-{ osConfig, ... }:
+{ osConfig, config, ... }:
 {
   programs.neovim = {
     enable = true;
@@ -6,5 +6,9 @@
   home.file."${osConfig.system.xdg.configDir}/nvim" = {
     source = ./neovim;
     recursive = true;
+  };
+
+  home.file."${osConfig.system.xdg.configDir}/nvim/lazy-lock.json" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${osConfig.system.dotfilesDir}/home/base/tui/neovim/lazy-lock.json";
   };
 }
