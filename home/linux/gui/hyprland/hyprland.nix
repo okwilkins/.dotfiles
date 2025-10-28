c7cae627d6db1c78fa326b32a188201735f136cb
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-28 21:21:38 +0000

    feat(hpyrland): Catppuccin theme download

--- home/linux/gui/hyprland/hyprland.nix
+++ home/linux/gui/hyprland/hyprland.nix
@@ -1,4 +1,12 @@
 { osConfig, pkgs, ... }:
+let
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "hyprland";
+    rev = "v1.3";
+    hash = "sha256-jkk021LLjCLpWOaInzO4Klg6UOR4Sh5IcKdUxIn7Dis=";
+  };
+in
 {
   home.packages = with pkgs; [
     hyprpaper
@@ -8,6 +16,10 @@
   ];
   home.file."${osConfig.system.xdg.configDir}/hypr/" = {
     source = ./config;
+    recursive = true;
+  };
+  home.file."${osConfig.system.xdg.configDir}/hypr/mocha.conf" = {
+    source = "${catppuccinRepo}/themes/mocha.conf";
     recursive = true;
   };
 }
