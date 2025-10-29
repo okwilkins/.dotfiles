68a14a285432b9d9032fbf5486ceeb6f81af634c
Oliver Wilkins <okwilkins@googlemail.com>
2025-10-29 18:34:47 +0000

    fix(outputs/x86_64-linux): Both Oak and Birch returns

--- outputs/x86_64-linux/default.nix
+++ outputs/x86_64-linux/default.nix
@@ -5,11 +5,23 @@
   overlays,
   ...
 }:
-import ./systems/home-oak.nix {
-  inherit
-    inputs
-    nixpkgs
-    home-manager
-    overlays
-    ;
-}
+
+nixpkgs.lib.recursiveUpdate
+  (import ./systems/home-birch.nix {
+    inherit
+      inputs
+      nixpkgs
+      home-manager
+      overlays
+      ;
+  })
+  (
+    import ./systems/home-oak.nix {
+      inherit
+        inputs
+        nixpkgs
+        home-manager
+        overlays
+        ;
+    }
+  )
