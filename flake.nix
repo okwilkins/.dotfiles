6e9552f97ca5a9ca0cabb759bdad763d13dc566e
Oliver Wilkins <okwilkins@googlemail.com>
2025-11-09 15:10:37 +0000

    feat: Dagger flake input

--- flake.nix
+++ flake.nix
@@ -19,6 +19,10 @@
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    dagger = {
+      url = "github:dagger/nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: import ./outputs inputs;
