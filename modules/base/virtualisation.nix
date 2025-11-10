ece052be3fde6c7bb618d5f672b377c3c3ee979e
Oliver Wilkins <okwilkins@googlemail.com>
2025-11-10 16:31:26 +0000

    refactor(modlues/virtualisation): Remove rootless mode

--- modules/base/virtualisation.nix
+++ modules/base/virtualisation.nix
@@ -1,20 +1,5 @@
 { ... }:
 {
-  virtualisation.docker = {
-    # Consider disabling the system wide Docker daemon
-    enable = false;
-
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-      # Optionally customize rootless Docker daemon settings
-      daemon.settings = {
-        dns = [
-          "1.1.1.1"
-          "8.8.8.8"
-        ];
-        registry-mirrors = [ "https://mirror.gcr.io" ];
-      };
-    };
-  };
+  virtualisation.docker.enable = true;
+  users.users.oli.extraGroups = [ "docker" ];
 }
