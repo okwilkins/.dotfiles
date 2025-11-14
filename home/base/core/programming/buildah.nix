a7eafbe128204922ada5af0acf6243a7e164e7f6
Oliver Wilkins <okwilkins@googlemail.com>
2025-11-14 21:37:14 +0000

    feat(buildah): Install and registries and config files

---
+++ home/base/core/programming/buildah.nix
@@ -0,0 +1,51 @@
+{
+  pkgs,
+  osConfig,
+  registryHost,
+  ...
+}:
+let
+  tomlFormat = pkgs.formats.toml { };
+  registryConfig = {
+    unqualified-search-registries = [
+      "docker.io"
+      "ghcr.io"
+    ];
+    registry = [
+      {
+        prefix = "docker.io";
+        location = "${registryHost}/docker-hub-cache";
+        mirror = [
+          {
+            location = "${registryHost}/docker-hub-cache";
+            insecure = false;
+          }
+        ];
+      }
+      {
+        prefix = "ghcr.io";
+        location = "${registryHost}/ghcr-cache";
+        mirror = [
+          {
+            location = "${registryHost}/ghcr-cache";
+            insecure = false;
+          }
+        ];
+      }
+    ];
+  };
+  registryToml = tomlFormat.generate "registry.conf" registryConfig;
+in
+{
+
+  home.packages = with pkgs; [
+    buildah
+  ];
+
+  home.file."${osConfig.system.xdg.configDir}/containers/policy.json" = {
+    source = ./buildah/config.json;
+  };
+  home.file."${osConfig.system.xdg.configDir}/containers/registry.conf" = {
+    source = registryToml;
+  };
+}
