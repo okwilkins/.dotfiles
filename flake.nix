2d8f7bc03790ec84ea9f30933c993114d519c941
Oliver Wilkins <okwilkins@googlemail.com>
2025-11-30 21:41:29 +0000

    build: Import sops-nix

--- flake.nix
+++ flake.nix
@@ -15,6 +15,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
