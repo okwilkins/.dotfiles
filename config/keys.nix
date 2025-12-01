18e03b84a5773c5eeece594fafd2dc309e6472e1
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-01 19:03:42 +0000

    fix(config): Newline string removed

--- config/keys.nix
+++ config/keys.nix
@@ -10,8 +10,7 @@
       # racoon
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIH4AQrahpVqwh6eDbB71pBwGBh+tlLRFqTfXyuB+qtlTAAAABHNzaDo= okwilkins@googlemail.com"
       # raven
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBDTUkI3Zvgl91ULeoD5YjoN67ldfuRdc3aZuN3CKDhKAAAABHNzaDo= okwilkins@googlemail.com
-      "
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIBDTUkI3Zvgl91ULeoD5YjoN67ldfuRdc3aZuN3CKDhKAAAABHNzaDo= okwilkins@googlemail.com"
     ];
   };
 }
