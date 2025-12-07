7d247da3e96bca841c4a7e6e694bdb07745594a5
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-07 15:17:09 +0000

    feat(modules/linux): Allow U2F PAM login

--- modules/linux/base/security.nix
+++ modules/linux/base/security.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
+  };
+
+  # INFO: https://nixos.wiki/wiki/Yubikey#pam_u2f
+  # To add more security keys read here
+  security.pam.services = {
+    login.u2fAuth = true;
+    sudo.u2fAuth = true;
   };
 }
