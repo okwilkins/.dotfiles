62d9cb2dfb5df746cc8f838d5359d74c9a05e9f6
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-09 07:28:08 +0000

    feat(neovim): clang-format conform

--- home/base/tui/neovim/lua/config/plugins/conform.lua
+++ home/base/tui/neovim/lua/config/plugins/conform.lua
@@ -8,6 +8,7 @@ return {
         python = { "ruff" },
         nix = { "nixfmt" },
         zig = { "zigfmt" },
+        cpp = { "clang-format" },
     },
     format_on_save = {
         -- These options will be passed to conform.format()
