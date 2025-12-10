2d233ba767f0379fd2deaae8948686663798a917
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-10 20:28:40 +0000

    refactor(sops): Remove reference to pre-generated age key file

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -10,8 +10,6 @@
     age.sshKeyPaths = map (key: key.path) (
       lib.filter (k: k.type == "ed25519") config.services.openssh.hostKeys
     );
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    age.generateKey = true;
     gnupg.sshKeyPaths = [ ];
   };
 
