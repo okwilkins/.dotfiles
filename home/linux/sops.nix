a2ce7d43a1de6e76f8481fbee264a4b221906807
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-15 21:49:23 +0000

    feat(home/linus): Sops for home-manager

---
+++ home/linux/sops.nix
@@ -0,0 +1,47 @@
+{
+  inputs,
+  pkgs,
+  osConfig,
+  ...
+}:
+let
+  secretsDir = builtins.toString inputs.dot-secrets;
+  secretsFile = "${secretsDir}/secrets.yaml";
+
+  ageDir = "${inputs.dot-secrets}/keys/age";
+  ageKeyFiles = map (name: "${ageDir}/${name}") (builtins.attrNames (builtins.readDir ageDir));
+  ageKeyFile = pkgs.writeText "age-identities" (
+    builtins.concatStringsSep "\n" (map builtins.readFile ageKeyFiles)
+  );
+in
+{
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
+  home.packages = with pkgs; [
+    sops
+    age-plugin-fido2-hmac
+    pam_u2f
+  ];
+
+  home.file."${osConfig.system.xdg.configDir}/sops/age-keys" = {
+    source = ageKeyFile;
+  };
+
+  sops = {
+    age.sshKeyPaths = [
+      # "${osConfig.system.homeDir}/.ssh/id_racoon"
+      # "${osConfig.system.homeDir}/.ssh/id_raven"
+      "${osConfig.system.homeDir}/.ssh/oli"
+    ];
+    # age.keyFile = "${osConfig.system.xdg.configDir}/sops/age-keys";
+    defaultSopsFile = "${secretsFile}";
+  };
+
+  sops.secrets."yubico/u2f_keys" = {
+    path = "${osConfig.system.xdg.configHome}/Yubico/u2f_keys";
+  };
+
+  sops.secrets."talos/homelab" = {
+    path = "${osConfig.system.xdg.configHome}/talos/talosconfig";
+  };
+}
