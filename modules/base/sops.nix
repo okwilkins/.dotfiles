5b863c62c72beff69d9b7d574b8b46c8416d7b28
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-15 17:35:21 +0000

    refactor(modules/sops): Use host key to inject Yubikeys into userspace and remove non-user secrets

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -4,23 +4,24 @@
   lib,
   ...
 }:
+let
+  ageDir = "${inputs.dot-secrets}/keys/age";
+  ageKeyFiles = map (name: "${ageDir}/${name}") (builtins.attrNames (builtins.readDir ageDir));
+  ageKeysContent = builtins.concatStringsSep "\n" (map builtins.readFile ageKeyFiles);
+in
 {
+  environment.etc."sops/age/keys.txt" = {
+    mode = "0400";
+    text = ageKeysContent;
+    user = "root";
+  };
   sops = {
     defaultSopsFile = "${inputs.dot-secrets}/secrets.yaml";
+    age.keyFile = "/etc/sops/age/keys.txt";
     age.sshKeyPaths = map (key: key.path) (
       lib.filter (k: k.type == "ed25519") config.services.openssh.hostKeys
     );
     gnupg.sshKeyPaths = [ ];
-  };
-
-  sops.secrets."yubico/u2f_keys" = {
-    owner = config.system.username;
-    path = "${config.system.xdg.configHome}/Yubico/u2f_keys";
-  };
-
-  sops.secrets."talos/homelab" = {
-    owner = config.system.username;
-    path = "${config.system.xdg.configHome}/talos/config.yaml";
   };
 
   sops.secrets."ssh_keys/racoon" = {
