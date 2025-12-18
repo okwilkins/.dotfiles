6007cb057542dc489e0cfe23ea55c2fc9690fb55
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-18 22:09:01 +0000

    feat(waybar): Include scripts dir

--- home/linux/gui/waybar/waybar.nix
+++ home/linux/gui/waybar/waybar.nix
@@ -1,8 +1,14 @@
 { pkgs, osConfig, ... }:
 {
   home.packages = with pkgs; [ waybar ];
+
   home.file."${osConfig.system.xdg.configDir}/waybar" = {
     source = ./config;
+    recursive = true;
+  };
+
+  home.file."${osConfig.system.xdg.configDir}/waybar/scripts" = {
+    source = ./scripts;
     recursive = true;
   };
 }
