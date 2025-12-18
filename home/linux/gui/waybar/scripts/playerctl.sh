acf00623715b7461869d22075d88e3e23279e304
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-18 22:01:48 +0000

    feat(waybar): playerctl script
    add

---
+++ home/linux/gui/waybar/scripts/playerctl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FORMAT=$(printf '{{status}}\t{{playerName}}\t{{markup_escape(title)}}\t{{markup_escape(artist)}}')
+
+playerctl metadata --format "$FORMAT" -F | \
+while IFS=$'\t' read -r status player title artist; do
+    if [[ -z "$artist" ]]; then
+        tooltip="$player: $title"
+    else
+        tooltip="$player: $artist - $title"
+    fi
+
+    jq -n --unbuffered \
+        --arg text "$title" \
+        --arg tooltip "$tooltip" \
+        --arg alt "$status" \
+        --arg class "$status" \
+        '{text: $text, tooltip: $tooltip, alt: $alt, class: $class}'
+done
+
