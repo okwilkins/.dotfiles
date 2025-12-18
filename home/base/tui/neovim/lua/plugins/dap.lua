125d9fd5339946b1b7ddd8d7702a7a566a9a7d6a
Oliver Wilkins <okwilkins@googlemail.com>
2025-12-18 18:03:35 +0000

    feat(nvim): DAP use lldb executable

--- home/base/tui/neovim/lua/plugins/dap.lua
+++ home/base/tui/neovim/lua/plugins/dap.lua
@@ -7,17 +7,44 @@ return {
         },
         config = function()
             local dap = require("dap")
-            local dapui = require("dapui")
 
-            dapui.setup()
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
+            dap.adapters.lldb = {
+                type = "executable",
+                command = "lldb-dap",
+                name = "lldb",
+            }
+
+            dap.configurations.cpp = {
+                {
+                    name = "Launch file",
+                    type = "lldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                    end,
+                    cwd = "${workspaceFolder}",
+                    stopOnEntry = false,
+                    args = {},
+                },
+            }
+
+            dap.defaults.fallback.terminal_win_cmd = "50vsplit new"
+
+            local has_ui, ui = pcall(require, "dapui")
+            if has_ui then
+                ui.setup()
+                dap.listeners.before.attach.dapui_config = function()
+                    ui.open()
+                end
+                dap.listeners.before.launch.dapui_config = function()
+                    ui.open()
+                end
+                dap.listeners.before.event_terminated.dapui_config = function()
+                    ui.close()
+                end
+                dap.listeners.before.event_exited.dapui_config = function()
+                    ui.close()
+                end
             end
         end,
     },
