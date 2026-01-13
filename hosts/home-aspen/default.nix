32e0dc12e43f7f44741712b3b211201150cd6714
Oliver Wilkins <okwilkins@googlemail.com>
2026-01-13 20:44:42 +0000

    feat: Create Aspen system

---
+++ hosts/home-aspen/default.nix
@@ -0,0 +1,50 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.hostName = "aspen-nixos";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${config.system.username} = {
+    isNormalUser = true;
+    description = config.system.userFullname;
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    # shell = pkgs.zsh;
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
---
+++ outputs/x86_64-linux/systems/home-aspen.nix
@@ -0,0 +1,39 @@
+{
+  nixpkgs,
+  home-manager,
+  sops-nix,
+  inputs,
+  overlays,
+  ...
+}:
+{
+  nixosConfigurations.aspen = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit inputs; };
+    modules = [
+      ../../../hosts/home-aspen
+      ../../../modules/linux/base
+      ../../../config
+      ../../../generators/docker.nix
+      sops-nix.nixosModules.sops
+      (
+        { ... }:
+        {
+          nixpkgs.overlays = overlays;
+        }
+      )
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.extraSpecialArgs = { inherit inputs; };
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        # Backup conflicting files when switching to not cause errors
+        home-manager.backupFileExtension = "backup";
+        home-manager.users.oli.imports = [
+          ../../../home/base
+          ../../../home/linux
+        ];
+      }
+    ];
+  };
+}
