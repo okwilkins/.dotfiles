efae3a18f4d5aaf23b63123fea56ff47a4db6e1a
Oliver Wilkins <okwilkins@googlemail.com>
2026-05-05 09:15:49 +0100

    refactor(home/neovim): Use new homemanager 26.05 default behaviours

--- home/base/tui/neovim.nix
+++ home/base/tui/neovim.nix
@@ -2,6 +2,22 @@
 {
   programs.neovim = {
     enable = true;
+    # INFO:
+    # evaluation warning: The default value of `programs.neovim.withRuby` has changed from `true` to `false`.
+    #                     You are currently using the legacy default (`true`) because `home.stateVersion` is less than "26.05".
+    #                     To silence this warning and keep legacy behavior, set:
+    #                       programs.neovim.withRuby = true;
+    #                     To adopt the new default behavior, set:
+    #                       programs.neovim.withRuby = false;
+    withRuby = false;
+    # INFO:
+    # evaluation warning: The default value of `programs.neovim.withPython3` has changed from `true` to `false`.
+    #                     You are currently using the legacy default (`true`) because `home.stateVersion` is less than "26.05".
+    #                     To silence this warning and keep legacy behavior, set:
+    #                       programs.neovim.withPython3 = true;
+    #                     To adopt the new default behavior, set:
+    #                       programs.neovim.withPython3 = false;
+    withPython3 = false;
   };
 
   home.file."${osConfig.system.xdg.configDir}/nvim/lua" = {
