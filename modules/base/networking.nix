8237a44b513d51ae68e54bb206b4b6ff3535079e
Oliver Wilkins <okwilkins@googlemail.com>
2026-05-06 20:21:04 +0100

    feat(networking): Enable network manager and IPv6 VPN on dispatcher

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -1,6 +1,34 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.ssh = {
     knownHosts = config.knownHosts;
   };
+
+  networking.networkmanager = {
+    enable = true;
+    plugins = with pkgs; [
+      networkmanager-openvpn
+    ];
+    dispatcherScripts = [
+      {
+        # Disable IPv6 when a VPN is enabled as with NordVPN IPv6 is leaking as un-VPNed IP
+        # https://nixsanctuary.com/how-to-prevent-ipv6-leak-with-an-openvpn-client-on-gnu-linux/
+        source = pkgs.writeText "vpn-ipv6-block" ''
+          #!/bin/sh
+          case "$2" in
+            vpn-up)
+              ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.disable_ipv6=1 >/dev/null 2>&1
+              ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.disable_ipv6=1 >/dev/null 2>&1
+              ;;
+            vpn-down)
+              ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.all.disable_ipv6=0 >/dev/null 2>&1
+              ${pkgs.procps}/bin/sysctl -w net.ipv6.conf.default.disable_ipv6=0 >/dev/null 2>&1
+              ;;
+          esac
+        '';
+        type = "basic";
+      }
+    ];
+  };
+
 }
