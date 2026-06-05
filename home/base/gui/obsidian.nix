0d388a0cb527d999e79613061bcdeb39e74856a6
Oliver Wilkins <okwilkins@googlemail.com>
2026-06-05 17:33:28 +0100

    feat(obdisian): Flash Navigate and packge updates
    fix

--- home/base/gui/obsidian.nix
+++ home/base/gui/obsidian.nix
@@ -14,6 +14,30 @@ let
       config.allowUnfree = true;
     }).obsidian;
 
+  flashNavigation = pkgs.linkFarm "flash-navigation" [
+    {
+      name = "main.js";
+      path = pkgs.fetchurl {
+        url = "https://github.com/Erl-koenig/obsidian-flash-navigation/releases/download/1.0.8/main.js";
+        hash = "sha256-aKf9nbl3mBAovHpSEPxyDT2eu3AsW9b1UtbPWOllJl8=";
+      };
+    }
+    {
+      name = "manifest.json";
+      path = pkgs.fetchurl {
+        url = "https://github.com/Erl-koenig/obsidian-flash-navigation/releases/download/1.0.8/manifest.json";
+        hash = "sha256-XI0HYRieoq5DloNd4HOlNscj7JsFvdzgt1ej+0J4aV4=";
+      };
+    }
+    {
+      name = "styles.css";
+      path = pkgs.fetchurl {
+        url = "https://github.com/Erl-koenig/obsidian-flash-navigation/releases/download/1.0.8/styles.css";
+        hash = "sha256-BVuZZlVO5OOqsHurVw9NUEHjr0j+4V725Xtx9EGwG3w=";
+      };
+    }
+  ];
+
   relVaultPath = "Documents/projects/knowledge-system";
   vaultPath = "${osConfig.system.homeDir}/${relVaultPath}";
   repoUrl = "git@github.com:okwilkins/knowledge-system.git";
@@ -26,11 +50,12 @@ let
     };
     communityPlugins = [
       (pkgs.fetchzip {
-        url = "https://github.com/Vinzent03/obsidian-git/releases/download/2.35.2/obsidian-git-2.35.2.zip";
-        name = "obsidian-git";
+        url = "https://github.com/Vinzent03/obsidian-git/releases/download/2.38.3/obsidian-git-2.38.3.zip";
+        name = "Git";
         stripRoot = true;
-        hash = "sha256-6ys4mIdlAeqABo7zTIBaH86ImqD/ywEr/SskQ5y9+Pc=";
+        hash = "sha256-Bm0USEhNtRCNzt9cx7vI6mgHF+mvZEAzo9WJalBKD8U=";
       })
+      flashNavigation
     ];
     corePlugins = [
       "backlink"
